d25d5cd3644ecb39f104721e79d459efeede35fc
masso1987 <44272779+masso1987@users.noreply.github.com>
2019-04-09 17:41:35 +0200

    Update tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+var fTemp = fahrenheit;
+  var fToCel.toFixed = (fTemp - 32) * 5 / 9;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+var cTemp = celsius;
+var cToFahr.toFixed = cTemp * 9/5 +32;
 }
 
 module.exports = {
