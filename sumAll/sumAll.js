08cb3da76531341a717d0f285969f3f49cf2ac96
masso1987 <44272779+masso1987@users.noreply.github.com>
2019-04-09 16:01:44 +0200

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(arr) {
+var start = Math.min (arr[0], arr[1]);
+var start = Math.max (arr[0], arr[1]);
+var total = 0;
+  for (var i=start; i<=end; i++){
+    total += i;
+  }
+  return total;
 }
 
 module.exports = sumAll
